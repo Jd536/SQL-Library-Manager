e8dd3991df19fc2e869fed7820604c5a84da2daf
Jimmy D <jd9192016@gmail.com>
2019-08-06 22:03:41 -0400

    removed op

--- routes/books.js
+++ routes/books.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var Books = require("../models").Books;
-const Op = Sequelize.Op;
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
